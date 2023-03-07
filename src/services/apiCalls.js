08cbcdede114c669fa7ae74934b12d98395fce15
JaviCMDev <capillaj.dev@gmail.com>
2023-03-07 18:32:06 +0100

    Vista basica Register con funcion register

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -4,6 +4,9 @@ const root = 'http://localhost:5500/';
 export const postLogin = async (credenciales) => {
     return await axios.post(`${root}users/login`, credenciales);
 };
+export const postRegister = async (credenciales) => {
+    return await axios.post(`${root}users/newuser`, credenciales);
+};
 
 export const getMythicplus = async () => {
     return await axios.get(`${root}mythicplus/getall`);
